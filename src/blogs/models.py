ec9423ef35f11b5e60663582d1ed3e84c1d84f70
hmhuan <minhhuanhuynh289@gmail.com>
2021-08-16 00:10:22 +0900

    add blogs models and migrate

--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    content = models.TextField()
+    
